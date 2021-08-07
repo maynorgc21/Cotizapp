55049f106a990d69369c95bb3d6022d8cedca8f7
Maynor <2243511170101@ingenieria.usac.edu.gt>
2021-08-07 12:55:44 -0600

    EJemplo carros y pasos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,14 @@
         background-color:#FFCC00;
         border-color:#FFCC00;
       }
+      .btn-violeta-suave{
+        background-color:#b591f9;
+        border-color:#FFCC00;
+      }
+      .btn-violeta-suave:hover{
+        background-color:#fff;
+        border-color:#fff;
+      }
       .btn-amarillo:hover{
         background-color:#fff;
         border-color:#fff;
@@ -48,6 +56,9 @@
         border-top-right-radius:45px;
         border-bottom-right-radius:45px;
       }
+      .ver:hover{
+        color:#FFCC00 !important;
+      }
     </style>
     <script src="https://kit.fontawesome.com/2a5eeeb7ec.js" crossorigin="anonymous"></script>
   </head>
@@ -69,7 +80,7 @@
 </nav>
 <section id="espacio" style="height: 125px; background-color: rgba(255,0,0,0.1);">
 </section>
-<section id="banner" class="banner" style="height: 100vh;">
+<section id="banner" class="banner p-4" style="height: 100%;">
 <div class="container">
 <div class="row"><br>
     </div>
@@ -106,19 +117,51 @@
     <div class="col"></div>
     </div>
     <div class="row">
-      <div class="col-md-4 col-sm-6">
-      <p class="nav-link text-amarillo">Quiero vender mi carro</p>
-      <button class="btn btn-outline-success btn-amarillo " type="submit">Vender</button>
+      <div class="col-md-4 col-sm-12">
+      <button class="btn btn-outline-success btn-violeta-suave btn-lg text-amarillo " type="submit">Vender</button>
       </div>
-      <div class="col-md-4 col-sm-6">
-      <p class="nav-link text-amarillo" >Quiero comprar un carro</p>
-      <button class="btn btn-outline-success btn-amarillo" type="submit">Comprar</button>
+      <div class="col-md-4 col-sm-12">
+      <button class="btn btn-outline-success btn-amarillo text-violeta btn-lg" type="submit">Comprar</button>
 
       </div> 
      </div>
 </div>
 </section>
- 
+<section id="descubrimiento" class="descubrimiento p-3">
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+      <h4 class="text-violeta"> Descubrimiento semanal </h4>
+    </div>
+    <div class="col">
+      <a href="#" class="text-violeta nav-link ver"> Ver Más > </a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col text-center"><img class="text-center" src="{{asset('/img/carroejemplo.jpg')}}" width="75%" alt=""><h5 class="text-violeta">Suzuki Alto</h5><p class="text-violeta">2020</p></div>
+    <div class="col text-center"><img src="{{asset('/img/carroejemplo.jpg')}}" width="75%" alt=""><h5 class="text-violeta">Mazda </h5><span class="text-violeta"> 2010</span></div>
+    <div class="col text-center"><img src="{{asset('/img/carroejemplo.jpg')}}" width="75%" alt=""><h5 class="text-violeta">Toyota Agia</h5><span class="text-violeta">2012</span></div>
+    <div class="col text-center"><img src="{{asset('/img/carroejemplo.jpg')}}" width="75%" alt=""><h5 class="text-violeta">Suzuki Jinmy</h5><span class="text-violeta">2018</span></div>
+  </div><br>
+  <div class="row">
+    <div class="col">
+    <h4 class="text-center text-violeta"> ¿Cómo hago mi compra financiada?</h4>
+    </div>
+  </div> <br>
+  <div class="row d-flex ">
+    <div class="col text-center"><img src="{{asset('/img/icono1.png')}}" width="50%" alt=""><h4 class="text-amarillo"> 1.<span class="text-violeta">Busca el carro que te guste</span></h4></div>
+    <div class="col text-center"><img src="{{asset('/img/icono2.png')}}" width="50%" alt=""><h4 class="text-amarillo"> 2.<span class="text-violeta">Precalifica</span></h4></div>
+    <div class="col text-center"><img src="{{asset('/img/icono3.png')}}" width="50%" alt=""><h4 class="text-amarillo"> 3.<span class="text-violeta">Agenda Cita</span></h4></div>
+  </div>
+</div>
+</section>
+<div class="card  text-white">
+  <img class="card-img" src="{{asset('/img/paso4.png')}}" alt="Card image">
+  <div class="card-img-overlay">
+  <h3 class="display-4 card-title text-center text-amarillo"> 4.<span class="text-white">¡Disfruta tu nuevo carro!</span></h3>
+
+  </div>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
